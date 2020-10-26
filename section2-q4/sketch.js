ae3d33e9057391f98e4547806e589f2357ec804f
haruchi27 <72755894+haruchi27@users.noreply.github.com>
2020-10-27 02:52:28 +0900

    計算で図形を描く課題

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,15 +1,19 @@
 // ギリシャ国旗
 function setup() {
-  const blue = color(0, 51, 160);
+  const blue = color(13, 94, 175);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+    if(i%2==0){
+      fill(blue);
+      rect(0, i * d, width, d);
+    }
+    else{fill(255);}
   }
 
   fill(blue);
@@ -17,5 +21,9 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+
+  rect(size/2-d/2,0,d, size)
+  rect(0,size/2-d/2,size, d)
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
+//ルーレット応用したらインドの旗風ができるかなって思ったんですけど限界でした。
