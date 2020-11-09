863a98169d8e827ba2949a1174759045293d7dc5
haruchi27 <72755894+haruchi27@users.noreply.github.com>
2020-11-10 07:33:21 +0900

    3-3以外の課題

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,28 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon(" I love nogizaka46 ");
+  //regularPolygon(10,10,10,10)
 }
 
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
+  push();
+  strokeWeight(1)
+  noFill();
+  ellipse(53, 13, w + p * 3, h + p * 3);
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
   text(t, p, h + p);
 }
+
+//function regularPolygon(n,cx, cy, r){
+  strokeWeight(1)
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 6 ;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
