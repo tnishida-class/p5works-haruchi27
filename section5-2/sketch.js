cdd3d60aba617f82114ffdd732b3d5e706feaca6
haruchi27 <72755894+haruchi27@users.noreply.github.com>
2020-11-24 03:29:26 +0900

    最終課題
    松松が入所してからデビューするまでの道のりを迷路で楽しむことができます。マウスをドラッグしながらゴールまで辿り着きましょう

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -26,7 +26,7 @@ function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(10,100), vx: dx, vy: dy };
     balls.push(b);
   }
 }
